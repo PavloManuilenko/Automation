d4ae0d0af52f8108f37ad43635b99fb02a7e62a9
PavloManuilenko <paulmanuilenko@gmail.com>
2017-06-29 12:11:50 +0300

    Add lesson 3
    Add lesson 3

--- lesson03/src/main/java/ua/dou/tests/FirstTestSuite.java
+++ lesson03/src/main/java/ua/dou/tests/FirstTestSuite.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class FirstTestSuite {
-    private static WebDriver driver;
+    private WebDriver driver;
     private HomePage homePage;
     private ForumPage forumPage;
     private NewsFeedPage newsFeedPage;
