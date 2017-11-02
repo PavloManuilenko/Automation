59242a209595fa02f6b2463c20d1ed8834d255bc
Pavlo <paulmanuilenko@gmail.com>
2017-11-02 19:15:12 +0200

    Adding the fix of the issue with US #11 and #12

--- GoogleTranslate/src/test/java/Feature3.java
+++ GoogleTranslate/src/test/java/Feature3.java
@@ -8,8 +8,6 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 public class Feature3 {
 
